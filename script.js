8589f51ea473321ca40f1c8c267b00d5fea5eaec
samjmccormick <samjmccormick@gmail.com>
2024-08-26 16:40:50 -0700

    function clean input string removes all characters except letters
    function checkPalindromeArray checks if an array is palindromic (is that a word?)
    
    combining those two functions and using split to turn a string into an array gives a working palindrome checker for strings. It will output false if it isn't a palindrome, otherwise it will output undefined.

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 const textInput = document.getElementById("text-input");
-const checkButton = document.getElementbyId("check-button");
+const checkButton = document.getElementById("check-button");
+
+/* const cleanInputString = (str) => {
+	const regex = /[^abcdefghinklmnopqrstuvwxyz]/gi;
+	const cleanString = str.replace(regex, '');
+}; 
+
+this was giving me an error because it 'replace' only works on strings and it doesn't know str is a string until an argument is passed, the new 
+function below is giving the same error, but correctly outputs to the console
+
+*/
+
+function cleanInputString (str) {
+	const regex = /[^abcdefghijnklmnopqrstuvwxyz]/gi;
+	return str.replace(regex, '');
+	/* tested and this removes all characters other than letters, including spaces */
+}
+
+function checkPalindromeArray (array) {
+	for (let i = 0; i < (array.length / 2); i++) {
+		if (array[i] !== array[array.length - i - 1]) {
+			return false 
+	} 
+	} 
+}
+
+
+function checkPalindromeString (str) {
+	const cleanString = cleanInputString (str);
+	const stringArray = cleanString.split("");
+	return checkPalindromeArray(stringArray);
+}
+
+
+console.log(checkPalindromeString("testtset"));
+
+
+checkButton.addEventListener("click", cleanInputString(textInput));
